1f3f470d5cbd3def82e799fe6b33ea8d267a27b0
Aaron Mangum <potatochip@users.noreply.github.com>
2015-04-30 16:42:19 -0400

    sample data

--- table_maker.py
+++ table_maker.py
@@ -9,7 +9,7 @@ import csv
 
 def file_reader(filename, type='csv'):
     data_file = []
-    if type='csv':
+    if type == 'csv':
         with open(filename, "rb") as f:
             reader = csv.reader(f, delimiter=' ', quotechar='|', quoting = csv.QUOTE_NONNUMERIC)
             headers = reader.next()
@@ -19,54 +19,56 @@ def file_reader(filename, type='csv'):
         except "unknown file type"
     return data_file, headers
 
-def connectdb(dbname, user):
-    conn = psycopg2.connect("dbname="+dbname+" user="+user)
-    cur = conn.cursor()
-    return conn, cur
+print file_reader("")
 
-def commit_and_closedb(cursor, connection):
-    conn.commit()
-    cursor.close()
-    connection.close()
+# def connectdb(dbname, user):
+#     conn = psycopg2.connect("dbname="+dbname+" user="+user)
+#     cur = conn.cursor()
+#     return conn, cur
 
-def create_table(cursor, data, table_name, headers, convert_numeric=True, convert_dates=True):
-    column_type_list = []
-    for item in data_file[0]:
-        if type(item) is float:
-            column_list.append("double precision")
-        elif type(item) is str:
-            column_list.append("varchar")
-        else:
-            except "unknown item type"
-    sql_statement = ", ".join( [str(j)+" "str(k) for j,k in zip(headers, column_list)] )
-    cur.execute("CREATE TABLE {0} ({1});".format(table_name, sql_statement))
+# def commit_and_closedb(cursor, connection):
+#     conn.commit()
+#     cursor.close()
+#     connection.close()
 
-def fill_table(data, table_name):
-    pass
+# def create_table(cursor, data, table_name, headers, convert_numeric=True, convert_dates=False):
+#     column_type_list = []
+#     for item in data_file[0]:
+#         if type(item) is float:
+#             column_list.append("double precision")
+#         elif type(item) is str:
+#             column_list.append("varchar")
+#         else:
+#             except "unknown item type"
+#     sql_statement = ", ".join( [str(j)+" "str(k) for j,k in zip(headers, column_list)] )
+#     cur.execute("CREATE TABLE {0} ({1});".format(table_name, sql_statement))
 
-def main():
-    try:
-        dbname = sys.argv[1]
-    except:
-        dbname = raw_input("Database name: ")
-    try:
-        user = sys.argv[2]
-    except:
-        user = raw_input("User name: ")
-    try:
-        filename = sys.argv[3]
-    except:
-        filename = raw_input("File name: ")
-    try:
-        table_name = sys.argv[4]
-    except:
-        table_name = raw_input("New table name: ")
+# def fill_table(data, table_name):
+#     pass
 
-    data, headers = file_reader(filename)
-    cursore, connection = connectdb(dbname, user)
-    create_table(cursor, data, table_name, headers)
-    fill_table(data, table_name)
-    commit_and_closedb(cursor, connection)
+# def main():
+#     try:
+#         dbname = sys.argv[1]
+#     except:
+#         dbname = raw_input("Database name: ")
+#     try:
+#         user = sys.argv[2]
+#     except:
+#         user = raw_input("User name: ")
+#     try:
+#         filename = sys.argv[3]
+#     except:
+#         filename = raw_input("File name: ")
+#     try:
+#         table_name = sys.argv[4]
+#     except:
+#         table_name = raw_input("New table name: ")
 
-if __name__ == "__main__":
-    main()
+#     data, headers = file_reader(filename)
+#     cursore, connection = connectdb(dbname, user)
+#     create_table(cursor, data, table_name, headers)
+#     fill_table(data, table_name)
+#     commit_and_closedb(cursor, connection)
+
+# if __name__ == "__main__":
+#     main()
